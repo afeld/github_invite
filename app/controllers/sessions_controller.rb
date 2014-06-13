cd87221bf9c10ad9218b522a0182da337a6e4f74
Aidan Feldman <afeld@github.com>
2014-06-12 21:23:13 -0400

    move current_user() method into ApplicationController

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,8 +15,4 @@ class SessionsController < ApplicationController
   def current_user=(user)
     session[:user_id] = user.id
   end
-
-  def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
 end
