6184bdd41c2a0e358016782d435a40dd0b6a21ab
Aidan Feldman <afeld@github.com>
2014-06-13 00:20:21 -0400

    store the team name and organization info when the team ID is assigned

--- app/models/invite.rb
+++ app/models/invite.rb
@@ -4,11 +4,24 @@ class Invite < ActiveRecord::Base
   validate :team_id, presence: true
   validate :user_id, presence: true
 
+  before_validation :update_info_if_team_changed
+
   def team
     @team ||= user.client.team(team_id)
   end
 
   def organization
     @organization ||= team.organization
+  end
+
+
+  protected
+
+  def update_info_if_team_changed
+    if team_id && team_id_changed?
+      self.organization_id = organization.id
+      self.organization_name = organization.login
+      self.team_name = team.name
+    end
   end
 end
